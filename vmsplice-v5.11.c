13c540dd00513e0ee7c0aa1866bab005a48ae3b4
Andrea Arcangeli <aarcange@redhat.com>
2021-01-10 23:05:14 -0500

    MM kernel-testcases-for-v5.11

---
+++ vmsplice-v5.11.c
@@ -0,0 +1,58 @@
+/*
+ * modified version of vmsplice testcase submitted in May 2020
+ * From: Jann Horn <jannh@google.com>
+ * fixed in 17839856fd588f4ab6b789f482ed3ffd7c403e1f but reintroduced
+ * and still affects v5.11-rc2 commit 71c061d2443814de15e177489d5cc00a4a253ef3
+ */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <err.h>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
+#define SYSCHK(x) ({          \
+  typeof(x) __res = (x);      \
+  if (__res == (typeof(x))-1) \
+    err(1, "SYSCHK(" #x ")"); \
+  __res;                      \
+})
+
+static void *data;
+
+static void child_fn(void) {
+  int pipe_fds[2];
+  SYSCHK(pipe(pipe_fds));
+  struct iovec iov = {.iov_base = data, .iov_len = 2*1024*1024 };
+  SYSCHK(vmsplice(pipe_fds[1], &iov, 1, 0));
+  SYSCHK(munmap(data, 2*1024*1024));
+  sleep(2);
+  char buf[2*1024*1024];
+  SYSCHK(read(pipe_fds[0], buf, 2*1024*1024));
+  printf("read string from child: %s\n", buf);
+}
+
+int main(void) {
+  if (posix_memalign(&data, 2*1024*1024, 2*1024*1024))
+    errx(1, "posix_memalign()");
+  if (madvise(data, 2*1024*1024, MADV_HUGEPAGE))
+    errx(1, "madvise()");
+  strcpy(data, "BORING DATA");
+
+  pid_t child = SYSCHK(fork());
+  if (child == 0) {
+    child_fn();
+    return 0;
+  }
+
+  sleep(1);
+  strcpy(data, "THIS IS SECRET");
+
+  int status;
+  SYSCHK(wait(&status));
+}
