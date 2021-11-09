eee8a5bddb8d3fe43ca2d54e68c4a95d05c8698b
David Hildenbrand <david@redhat.com>
2021-11-09 10:26:58 +0100

    Add simple FOLL_PIN memory corruption reproducer using io_uring fixed buffers
    io_uring with fixed buffers uses FOLL_PIN | FOLL_WRITE | FOLL_LONGTERM
    to pin the buffer pages. The interesting thing is that this is a race
    between checking if a page is pinned and putting it into the swap cache
    and concurrent pinning activity.
    
    The same issue could be easily hit in practice when starting a VM and
    hotplugging a VFIO device: while hotplugging the VFIO device, we'll pin
    all guest memory that might already be used by the running VM. If we
    have concurrent swap activity, we're going to have a bad time if we
    accidentially COW a page and lose synchronicity between the VFIO MMU and
    the process page tables.
    
    Note that reproducing might take a while as there is a relatively small
    race window and unregistering io_uring buffers takes forever.
    
    To reproduce somewhat quickly, I'm running the test in a VM:
    
    VM: QEMU VM with 4 VCPUs, 2 GiB of RAM and > 1 GiB swap enabled on a
        virtio-blk disk. 5.14.16-101.fc33.x86_64.
    Hypervisor: 12 core (24 threads) AMD Ryzen 9 3900X with 64 GiB of RAM.
    
    Will allocate 2989 MiB in order to swap
    Reading attempt #1
    Reading attempt #2
    Reading attempt #3
    Reading attempt #4
    Reading attempt #5
    Reading attempt #6
    memory corruption detected
    
    Will allocate 2989 MiB in order to swap
    Reading attempt #1
    Reading attempt #2
    Reading attempt #3
    ...
    Reading attempt #113
    Reading attempt #114
    memory corruption detected
    
    Will allocate 2989 MiB in order to swap
    Reading attempt #1
    Reading attempt #2
    Reading attempt #3
    Reading attempt #4
    Reading attempt #5
    ...
    Reading attempt #57
    Reading attempt #58
    memory corruption detected
    
    Will allocate 2989 MiB in order to swap
    Reading attempt #1
    Reading attempt #2
    Reading attempt #3
    Reading attempt #4
    Reading attempt #5
    ...
    Reading attempt #91
    Reading attempt #92
    memory corruption detected
    
    It seems to triggers less likely on my 16 GiB notebook:
    
    Will allocate 16894 MiB in order to swap
    Reading attempt #1
    Reading attempt #2
    Reading attempt #3
    Reading attempt #4
    ...
    Reading attempt #151
    Reading attempt #152
    memory corruption detected
    
    And even less likely on my 64 GiB workstation:
    
    $ ./io_uring_swap whateverfile
    Will allocate 65246 MiB in order to swap
    Reading attempt #1
    Reading attempt #2
    Reading attempt #3
    Reading attempt #4
    Reading attempt #5
    ...
    Reading attempt #411
    Reading attempt #412
    memory corruption detected
    
    ... because it's less likely that we'll actually go ahead and swap out
    the single target page at the right point in time the more physical
    memory we have installed.
    
    Signed-off-by: David Hildenbrand <david@redhat.com>

---
+++ io_uring_swap.c
@@ -0,0 +1,251 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ *  reproducer for v5.11 (still works on v5.15-rc3) memory corruption
+ *  with page_count instead of mapcount in do_wp_page with only
+ *  io uring fixed read and swap.
+ *
+ *  Copyright (C) 2021  Red Hat, Inc.
+ *
+ *  gcc -O2 -o io_uring_swap io_uring_swap.c -lpthread -luring
+ *  ./io_uring_swap ./whateverfile
+ *
+ *  NOTE: swap must be enabled. The smaller the total memory in the system
+ *  the easier it is to reproduce. Inside a 2 GiB VM it triggers fairly
+ *  reliably within minutes.
+ *
+ *  This is caused by the VM design flaw introduced in commit
+ *  09854ba94c6aad7886996bfbee2530b3d8a7f4f4.
+ *
+ *  Fixed in https://github.com/aagit/aa/tree/mapcount_unshare
+ */
+
+#define _GNU_SOURCE
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <sys/errno.h>
+#include <sys/syscall.h>
+#include <sys/mman.h>
+#include "liburing.h"
+
+#define PAGE_SIZE (1UL<<12)
+/*
+ * NOTE: an arch with a PAGE_SIZE > 4k will reproduce the silent mm
+ * corruption with an HARDBLKSIZE of 4k or more.
+ */
+#define HARDBLKSIZE 512
+
+static void* writer(void *_mem)
+{
+	volatile char *mem = (char *)_mem;
+	char x;
+	for(;;) {
+		usleep(random() % 1000);
+		x = mem[PAGE_SIZE-1];
+		mem[PAGE_SIZE-1] = x;
+	}
+	return NULL;
+}
+
+static void* background_pageout(void *_mem)
+{
+	char *mem = (char *)_mem;
+	for(;;) {
+		usleep(random() % 1000);
+		madvise(mem, PAGE_SIZE, MADV_PAGEOUT);
+	}
+	return NULL;
+}
+
+static void* background_swap(void *_size)
+{
+	unsigned long size = (unsigned long) _size;
+	for (;;) {
+		volatile char *p = malloc(size);
+		if (!p)
+			perror("malloc"), exit(1);
+		for (unsigned long i = 0; i < size; i += PAGE_SIZE) {
+			p[i] = 0;
+		}
+		free((void *)p);
+	}
+	return NULL;
+}
+
+static int io_uring_read_fixed(struct io_uring *ring, int fd, void *buf,
+			       size_t size)
+{
+	static unsigned long count;
+	struct io_uring_cqe *cqe;
+	struct io_uring_sqe *sqe;
+	struct iovec iov;
+	int ret, res;
+
+	printf("Reading attempt #%lu\n", ++count);
+
+	iov.iov_base = buf;
+	iov.iov_len = size;
+
+	/*
+	 * Map the buffer, this will FOLL_PIN | FOLL_LONGTERM the target page.
+	 * If we happen to pin just after putting the page into the swap cache
+	 * and before unmapping it, we can be in trouble.
+	 */
+	ret = io_uring_register_buffers(ring, &iov, 1);
+	if (ret) {
+		fprintf(stderr, "io_uring_register_buffers() failed: %d\n",
+			ret);
+		return ret;
+	}
+
+	/*
+	 * Let's wait a bit until actually reading the content, such that any
+	 * wrong COW will see stale data.
+	 */
+	usleep(random() % 1000);
+
+	sqe = io_uring_get_sqe(ring);
+	if (!sqe) {
+		fprintf(stderr, "io_uring_get_sqe() failed: %d\n", ret);
+		return ret;
+	}
+	io_uring_prep_read_fixed(sqe, fd, buf, size, 0, 0);
+
+	ret = io_uring_submit(ring);
+	if (ret < 0) {
+		fprintf(stderr, "io_uring_submit() failed: %d\n", ret);
+		return ret;
+	}
+
+	ret = io_uring_wait_cqe(ring, &cqe);
+	if (ret < 0) {
+		fprintf(stderr, "io_uring_wait_cqe() failed: %d\n", ret);
+		return ret;
+	}
+
+	res = cqe->res;
+	io_uring_cqe_seen(ring, cqe);
+
+	/*
+	 * Unmap the buffer, this will unpin the target page. Unfortunately,
+	 * this might take a long time.
+	 */
+	ret = io_uring_unregister_buffers(ring);
+	if (ret) {
+		fprintf(stderr, "io_uring_unregister_buffers()\n");
+		return ret;
+	}
+	return res;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+		printf("%s <filename>\n", argv[0]), exit(1);
+
+	char *mem;
+	if (posix_memalign((void **)&mem, PAGE_SIZE, PAGE_SIZE*3))
+		perror("posix_memalign"), exit(1);
+
+	/* THP is not using page_count so it would not corrupt memory */
+	if (madvise(mem, PAGE_SIZE, MADV_NOHUGEPAGE))
+		perror("madvise"), exit(1);
+
+	bzero(mem, PAGE_SIZE * 3);
+	memset(mem + PAGE_SIZE * 2, 0xff, HARDBLKSIZE);
+
+	int fd = open(argv[1], O_CREAT|O_RDWR|O_TRUNC, 0600);
+	if (fd < 0)
+		perror("open"), exit(1);
+	if (write(fd, mem, PAGE_SIZE) != PAGE_SIZE)
+		perror("write"), exit(1);
+
+	FILE *file = fopen("/proc/meminfo", "r");
+	if (!file)
+		perror("fopen meminfo"), exit(1);
+
+	char *line = NULL;
+	size_t len = 0;
+	unsigned long mem_total = 0, mem_avail = 0, swap_total = 0, swap_free = 0;
+	int match = 0;
+	while (getline(&line, &len, file) > 0) {
+		if (sscanf(line, "MemTotal: %lu kB", &mem_total))
+			match++;
+		if (sscanf(line, "MemAvailable: %lu kB", &mem_avail))
+			match++;
+		if (sscanf(line, "SwapTotal: %lu kB", &swap_total))
+			match++;
+		if (sscanf(line, "SwapFree: %lu kB", &swap_free)) {
+			match++;
+			break;
+		}
+	}
+
+	/* Consume an additional 1 GiB */
+	unsigned long size_kb = mem_total + 1024*1024;
+
+	if (match != 4 || swap_free > swap_total)
+		fprintf(stderr, "/proc/meminfo error\n"), exit(1);
+	if (!swap_total || !swap_free || swap_free < size_kb - mem_avail)
+		fprintf(stderr, "not enough swap\n"), exit(1);
+
+	unsigned long size = size_kb * 1024;
+	printf("Will allocate %lu MiB in order to swap\n", size / 1024 / 1024);
+
+	pthread_t pageout;
+	if (pthread_create(&pageout, NULL, background_pageout, mem))
+		perror("pthread_create pageout"), exit(1);
+
+	pthread_t swap;
+	if (pthread_create(&swap, NULL, background_swap, (void *)size))
+		perror("pthread_create swap"), exit(1);
+
+	pthread_t thread;
+	if (pthread_create(&thread, NULL, writer, mem))
+		perror("pthread_create writer"), exit(1);
+
+	struct io_uring ring;
+	int ret = io_uring_queue_init(1, &ring, 0);
+	if (ret < 0) {
+		perror("io_uring_queue_init");
+		exit(ret);
+	}
+
+	bool skip_memset = true;
+	while (1) {
+		if (io_uring_read_fixed(&ring, fd, mem, HARDBLKSIZE) != HARDBLKSIZE) {
+			fprintf(stderr, "io_uring_read_fixed() failed\n");
+			exit(-1);
+		}
+		if (memcmp(mem, mem+PAGE_SIZE, HARDBLKSIZE)) {
+			if (memcmp(mem, mem+PAGE_SIZE*2, PAGE_SIZE)) {
+				if (skip_memset)
+					printf("unexpected memory "
+					       "corruption detected\n");
+				else
+					printf("memory corruption detected, "
+					       "dumping page\n");
+				int end = PAGE_SIZE;
+				if (!memcmp(mem+HARDBLKSIZE, mem+PAGE_SIZE,
+					    PAGE_SIZE-HARDBLKSIZE))
+					end = HARDBLKSIZE;
+				for (int i = 0; i < end; i++)
+					printf("%x", mem[i]);
+				printf("\n");
+			} else {
+				printf("memory corruption detected\n");
+				exit(-1);
+			}
+		}
+		skip_memset = !skip_memset;
+		if (!skip_memset)
+			memset(mem, 0xff, HARDBLKSIZE);
+	}
+
+	return 0;
+}
